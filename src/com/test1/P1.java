b9a00c8e8311bd1f4402e84e4cd1d06435781c31
Hema B <Hema B@DESKTOP-U9BDERS.lan>
2025-05-06 09:40:54 +0530

    error removed

--- src/com/test1/P1.java
+++ src/com/test1/P1.java
@@ -1,14 +1,16 @@
 package com.test1;
 
+import java.util.Scanner;
+
 public class P1 {
 	public static void main(String args[]) {
 		System.out.println("Hello World");
-		int num;
+		
 		float res=0;
-		Scanner sc = new Scanner (System.in);
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the number");
-		int num = scan.nextInt();
-		res = num *num;
-		System.out.println("the Square is")+ res;
+		int num1 = sc.nextInt();
+		res = num1 *num1;
+		System.out.println("the Square is"+ " "+ res);
 	}
 }
