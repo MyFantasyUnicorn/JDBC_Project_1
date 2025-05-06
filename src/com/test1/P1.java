e64e88618a12ef1fd9362bcc79c807eb7d9030aa
MyFantasyUnicorn <ramyabasavaraju21@gmail.com>
2025-05-06 09:31:53 +0530

    Update P1.java

--- src/com/test1/P1.java
+++ src/com/test1/P1.java
@@ -3,5 +3,12 @@ package com.test1;
 public class P1 {
 	public static void main(String args[]) {
 		System.out.println("Hello World");
+		int num;
+		float res=0;
+		Scanner sc = new Scanner (System.in);
+		System.out.println("Enter the number");
+		int num = scan.nextInt();
+		res = num *num;
+		System.out.println("the Square is")+ res;
 	}
 }
